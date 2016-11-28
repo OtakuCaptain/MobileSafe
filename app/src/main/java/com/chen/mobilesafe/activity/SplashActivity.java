c3ebd5755727ca63da01f13f1f435234adfe5687
OtakuCaptain <chen_t_y@hotmail.com>
2016-11-28 20:16:05 +0800

    splash

--- app/src/main/java/com/chen/mobilesafe/activity/SplashActivity.java
+++ app/src/main/java/com/chen/mobilesafe/activity/SplashActivity.java
@@ -1,15 +1,81 @@
 package com.chen.mobilesafe.activity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.chen.activity.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private TextView tv_version_name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+//        初始化UI
+        initUI();
+//       初始化数据
+        initData();
+
     }
+
+    /**
+     *
+     */
+    private void initData() {
+        //应用版本名称
+        tv_version_name.setText(getVersionName());
+        getVersionCode();
+
+    }
+
+    /**
+     * 获取版本号
+     * @return
+     *            返回0说明异常
+     */
+    private int getVersionCode() {
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    /**
+     * 获取版本名称
+     *
+     * @return 返回null说明异常
+     */
+    private String getVersionName() {
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * 初始化UI的方法
+     */
+    private void initUI() {
+        tv_version_name = (TextView) findViewById(R.id.tv_version_name);
+
+    }
+
+
+
 }
