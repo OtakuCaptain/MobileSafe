fba6a6f3fa9d5305203913725e31fa84fb604a00
OtakuCaptain <chen_t_y@hotmail.com>
2016-11-29 11:09:30 +0800

    解析json

--- app/src/main/java/com/chen/mobilesafe/activity/SplashActivity.java
+++ app/src/main/java/com/chen/mobilesafe/activity/SplashActivity.java
@@ -2,21 +2,47 @@ package com.chen.mobilesafe.activity;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
 import com.chen.activity.R;
 import com.chen.mobilesafe.utils.StreamUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class SplashActivity extends AppCompatActivity {
     protected static final String tag = "SplashActivity";
+    /**
+     * 更新版本状态码
+     */
+    private static final int UPDATE_VERSION = 100;
+
+    /**
+     * 直接进入主界面状态码
+     */
+    private static final int ENTER_HOME = 101;
 
     private TextView tv_version_name;
     private int mLocalVersionCode;
+
+    private Handler mHandler=new Handler(){
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +54,7 @@ public class SplashActivity extends AppCompatActivity {
 //       初始化数据
         initData();
     }
+
     /**
      *
      */
@@ -38,6 +65,7 @@ public class SplashActivity extends AppCompatActivity {
         mLocalVersionCode = getVersionCode();
         checkCode();
     }
+
     /**
      * 检测版本号
      */
@@ -45,9 +73,11 @@ public class SplashActivity extends AppCompatActivity {
 
         new Thread() {
             public void run() {
+
+                Message msg = Message.obtain();
                 try {
                     //封装URL地址
-                    URL url = new URL("http://192.168.162.2:8080/mobilesafe.json");
+                    URL url = new URL("http://192.168.0.118:8080/mobilesafe.json");
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
                     //设置请求头
@@ -62,9 +92,36 @@ public class SplashActivity extends AppCompatActivity {
                         InputStream inputStream = connection.getInputStream();
                         String json = StreamUtil.streamToString(inputStream);
 
-                        Log.i(tag,json);
+                        Log.i(tag, json);
+
+//                        解析json
+                        JSONObject jsonObject = new JSONObject(json);
+
+                        String versionName = jsonObject.getString("versionName");
+                        String versionDes = jsonObject.getString("versionDes");
+                        String versionCode = jsonObject.getString("versionCode");
+                        String downloadUrl = jsonObject.getString("downloadUrl");
+
+                        Log.i(tag, versionName);
+                        Log.i(tag, versionDes);
+                        Log.i(tag, versionCode);
+                        Log.i(tag, downloadUrl);
+
+                        if (mLocalVersionCode < Integer.parseInt(versionCode)) {
+                            //提示用户更新
+                            msg.what=UPDATE_VERSION;
+
+                        } else {
+                            //进入主界面
+                            msg.what=ENTER_HOME;
+
+                        }
                     }
-                } catch (Exception e) {
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
