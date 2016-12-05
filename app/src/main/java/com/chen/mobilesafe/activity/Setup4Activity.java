db373ebe7bb15e6d1a1d4fd98addca7720724fa4
OtakuCaptain <chen_t_y@hotmail.com>
2016-12-05 15:59:21 +0800

    导航界面布局完成

--- app/src/main/java/com/chen/mobilesafe/activity/Setup4Activity.java
+++ app/src/main/java/com/chen/mobilesafe/activity/Setup4Activity.java
@@ -6,27 +6,33 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.chen.activity.R;
+import com.chen.mobilesafe.utils.ConstantValue;
+import com.chen.mobilesafe.utils.SpUtil;
+
+import java.lang.reflect.AccessibleObject;
 
 /**
  * Created by chen on 2016-12-05.
  */
-public class Setup3Activity extends Activity{
+public class Setup4Activity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup3);
+        setContentView(R.layout.activity_setup4);
     }
+
     public void prePage(View v){
-        Intent intent = new Intent(this, Setup2Activity.class);
+        Intent intent = new Intent(this, Setup3Activity.class);
         startActivity(intent);
         finish();
 
     }
 
     public void nextPage(View v){
-        Intent intent = new Intent(this, Setup4Activity.class);
+        Intent intent = new Intent(this, SetupOverActivity.class);
         startActivity(intent);
         finish();
 
+        SpUtil.putBoolean(this, ConstantValue.SETUP_OVER,true);
     }
 }
